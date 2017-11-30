0649a8cab3496c257382393dfe3bd74e77b5b31d
72gsir <72gsir@gmail.com>
2017-12-01 01:07:56 +0300

    My first project on Unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    private int count;
+    // Text fields
+    public Text countText, winText;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        setCount();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cube")
+        {
+            // hidden
+            //other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+
+            count++;
+            setCount();
+        }
+    }
+
+    private void LateUpdate()
+    {
+    }
+
+
+    void setCount() {
+        countText.text = "Ваш счет: " + count.ToString();
+
+        if (count >= 4)
+        {
+            winText.text = "Вы выиграли!";
+        }
+
+    }
+
+
+}
